231a08ffa2b526b7993bdd563d24a1d65d94836f
이현우 <hwlee3928@gmail.com>
2021-06-16 18:49:12 +0900

    check validation doubly linked list  Changes to be committed: 	modified:   main.c

--- push_swap/srcs/main.c
+++ push_swap/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: hyunwlee <hyunwlee@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/04 16:13:31 by hyunwlee          #+#    #+#             */
-/*   Updated: 2021/06/16 02:17:03 by hyunwlee         ###   ########.fr       */
+/*   Updated: 2021/06/16 18:48:45 by hyunwlee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,22 +18,42 @@ int		main(int argc, char *argv[])
 {
 	// t_deque b_deque;
 	t_deque a_deque;
+
 	a_deque.head = NULL;
 	a_deque.tail = NULL;
 	a_deque.size = 0;
-
-	printf("%s\n", argv[1]);
 	if (argc < 2)
 	{
 		puts("INPUT ERROR");
 		exit(1);
 	}
 	for (int i = 1; i < argc; i++)
-		add_front(&a_deque, ft_atoi(argv[i]));
+		add_rear(&a_deque, ft_atoi(argv[i]));
 
-	printf("[a_deque] get_front: %d\n", get_front(&a_deque));
-	printf("[a_deque] get_rear: %d\n", get_rear(&a_deque));
+	printf("head_value: %d\n", a_deque.head->value);
+	printf("tail_value: %d\n", a_deque.tail->value);
 	printf("size : %d\n", a_deque.size);
+
+    // for (;a_deque.size != 0;)
+    // {
+    //     printf("%d\n", a_deque.size);
+	//     printf("[a_deque] delete_rear: %d\n\n", delete_rear(&a_deque));
+    // }
+    // printf("%d\n", a_deque.head->r_link->value);
+    // printf("%d\n", a_deque.head->r_link->r_link->value);
+    // printf("%d\n", a_deque.head->r_link->r_link->r_link->value);
+    // printf("%d\n", a_deque.head->r_link->r_link->r_link->r_link->value);
+    // printf("%d\n", a_deque.head->r_link->r_link->r_link->r_link->r_link->value);
+    // printf("%d\n", a_deque.head->r_link->r_link->r_link->r_link->r_link->r_link->value);
+	// puts("");
+    // printf("%d\n", a_deque.tail->r_link->value);
+    // printf("%d\n", a_deque.tail->r_link->r_link->value);
+    // printf("%d\n", a_deque.tail->r_link->r_link->r_link->value);
+    // printf("%d\n", a_deque.tail->r_link->r_link->r_link->r_link->value);
+    // printf("%d\n", a_deque.tail->r_link->r_link->r_link->r_link->r_link->value);
+    // printf("%d\n", a_deque.tail->r_link->r_link->r_link->r_link->r_link->r_link->value);
+	
+    // printf("[a_deque] delete_front: %d\n", delete_front(&a_deque));
 	/////////////////////////////////////////////////////////
 	// add_rear(&b_deque, 3);
 	// add_rear(&b_deque, 1);
